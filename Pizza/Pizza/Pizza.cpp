3976582194931ae17659fa9bc761dab7652f1945
CharlesCarlstrom <cc113884@students.davenport.k12.ia.us>
2017-04-10 10:36:43 -0500

    Update

--- Pizza/Pizza/Pizza.cpp
+++ Pizza/Pizza/Pizza.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include<string>
 using namespace std;
-
+///Dipesh Thapa
+///Charles Carlstrom
 
 int main()
 {
 	int pizzaYes = 1, pizzaNo = 2, small = 1, medium = 2, large = 3;
 	const float pizzalarge = 14.99, pizzamedium = 12.99, pizzasmall = 9.99;
-	int pizzaNumOrdered ,orderInput;
+	int pizzaNumOrdered, orderInput;
 	float pizzaTotal;
+	string topping;
 
-	
+
 	cout << "Welcome to ______ Pizza";
 	cout << "We have a wide range of pizzas to choose from,\n"
 		"would you like to order a pizza? 1 for yes, 2 for no.";
@@ -22,21 +25,99 @@ int main()
 	{
 		cout << "I am glad you would like a pizza, what size? 1 for small, 2 for medium, and 3 for large";
 		cin >> orderInput;
-		
+
 		if (orderInput == 1)
-			pizzaNumOrdered = pizzasmall; 
-			cout << "Small pizza" <<pizzaNumOrdered << std::endl;
+		{
+
+			cout << "How many small pizza you want";
+			std::cin >> pizzaNumOrdered;
+			cout << "what toppings do you want: Pepperoni, sausage, or cheese?";
+			cin >> topping;
+
+			if (topping == "cheese" || topping == "Cheese")
+			{
+				pizzaTotal = pizzaNumOrdered * pizzasmall;
+				cout << " Your price for chesse pizza is $" << pizzaTotal << std::endl;
+				//do this statement
+
+			}
+			else if (topping == "pepperoni")
+			{
+				pizzaTotal = pizzaNumOrdered * pizzasmall;
+				cout << " Your price for pepperoni pizza is $" << pizzaTotal << std::endl;
+				//do this
+			}
+			else if (topping == "sausage")
+			{
+				pizzaTotal = pizzaNumOrdered * pizzasmall;
+				cout << " Your price for sausage is $" << pizzaTotal << std::endl;
+			}
+
+		}
+
+
+		else if (orderInput == 2)
+		{
+
+			cout << "How many Medium pizza you want";
+			std::cin >> pizzaNumOrdered;
+			cout << "what toppings do you want: Pepperoni, sausage, or cheese?";
+			cin >> topping;
+			if (topping == "cheese" || topping == "Cheese")
+			{
+				pizzaTotal = pizzaNumOrdered * pizzamedium;
+				cout << " Your price for chesse pizza is $" << pizzaTotal << std::endl;
+				//do this statement
+
+			}
+			else if (topping == "pepperoni")
+			{
+				pizzaTotal = pizzaNumOrdered * pizzamedium;
+				cout << " Your price for pepperoni pizza is $" << pizzaTotal << std::endl;
+				//do this
+			}
+			else if (topping == "sausage")
+			{
+				pizzaTotal = pizzaNumOrdered * pizzamedium;
+				cout << " Your price for sausage is $" << pizzaTotal << std::endl;
+			}
+		}
 		
 
 
-		 if (orderInput == 2)
-			pizzaNumOrdered = pizzamedium; 
-			cout << "Medium pizza" << pizzaNumOrdered << std::endl;
+		else if (orderInput == 3)
+		{
 
-		if (orderInput == 3)
-			pizzaNumOrdered = pizzalarge; 
-			cout << "large pizza" << pizzaNumOrdered << std::endl;
+			cout << "How many Large pizza you want";
+			std::cin >> pizzaNumOrdered;
+			cout << "what toppings do you want: Pepperoni, sausage, or cheese?";
+			cin >> topping;
+			if (topping == "cheese" || topping == "Cheese")
+			{
+				pizzaTotal = pizzaNumOrdered * pizzalarge;
+				cout << " Your price for chesse pizza is $" << pizzaTotal << std::endl;
+				//do this statement
 
+			}
+			else if (topping == "pepperoni")
+			{
+				pizzaTotal = pizzaNumOrdered *  pizzalarge;
+				cout << " Your price for pepperoni pizza is $" << pizzaTotal << std::endl;
+				//do this
+			}
+			else if (topping == "sausage")
+			{
+				pizzaTotal = pizzaNumOrdered *  pizzalarge;
+				cout << " Your price for sausage is $" << pizzaTotal << std::endl;
+			}
+
+		}
 
 	}
+	else if (pizzaNo == 2)
+	
+		cout << "Sorry to hear that please leave.";
+	
+
 }
+
